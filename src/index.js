b63b2d5eea3cfa5d0d175ef756b731f8ce44c5ea
saadAtIndex <saad@indexgroup.net>
2018-09-30 12:49:39 +0200

    fisr

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import config from './config.json';
+
+let app = express();
+app.server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'admin')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admin/index.html'));
+});
+
+app.use(bodyParser.json({
+	limit: config.bodyLimit
+}));
+
+app.server.listen(process.env.PORT || config.port, () => {
+	console.log(`Started on port ${app.server.address().port}`);
+});
+
+
+export default app;
